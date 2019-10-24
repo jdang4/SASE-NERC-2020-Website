89fc692fc1c66026b825a066f0dfbc9c7541cd05
Jonathan Dang <jdang@wpi.edu>
2019-10-24 19:28:28 -0400

    added in all the backgrounds and centered the nav bar

--- public/navBarCollapse.js
+++ public/navBarCollapse.js
@@ -1,3 +1,8 @@
 $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
+
+$( '.navbar-nav a' ).on( 'click', function () {
+	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+	$( this ).parent( 'li' ).addClass( 'active' );
+});
