4c43cc36d86bc317792dd6b2c0630a6e4c8a231c
Jonathan Dang <jdang@wpi.edu>
2019-11-01 13:06:16 -0400

    removed uneccessary codes

--- public/scripts.js
+++ public/scripts.js
@@ -12,15 +12,6 @@ $(document).ready(function(){
     $("SplashLogo").fadeIn(5000);
 });
 
-/*
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 'slow');
-    return false;
-});
-*/
-
 $(document).ready(function(){
   $('a[href^="#"]').on('click', function(event) {
     var target = $(this.getAttribute('href'));
@@ -36,13 +27,3 @@ $(document).ready(function(){
 
 $("a").removeClass("waves-effect waves-light");
 
-//get window height minus 50 pixels for the headers height
-var height = $(window).height() - 50;
-
-$(".main").height(height);
-
-window.onresize = function(event) {
-var height = $(window).height() - 50;
-
-$(".main").height(height);
-};
