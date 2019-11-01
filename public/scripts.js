7f5fd7350ac7a6e511eaf56533ebe9384291a59b
Jonathan Dang <jdang@wpi.edu>
2019-11-01 12:58:57 -0400

    fixed scrolling animation issue on mobile - needs testing

--- public/scripts.js
+++ public/scripts.js
@@ -1,22 +1,48 @@
+
 $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
 
-/*
 $( '.navbar-nav a' ).on( 'click', function () {
 	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 	$( this ).parent( 'li' ).addClass( 'active' );
 });
-*/
+
 $(document).ready(function(){
     $("SplashLogo").fadeIn(5000);
 });
 
+/*
 $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
-    }, 900);
+    }, 'slow');
     return false;
 });
+*/
+
+$(document).ready(function(){
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+  });
+});
+ 
 
 $("a").removeClass("waves-effect waves-light");
+
+//get window height minus 50 pixels for the headers height
+var height = $(window).height() - 50;
+
+$(".main").height(height);
+
+window.onresize = function(event) {
+var height = $(window).height() - 50;
+
+$(".main").height(height);
+};
