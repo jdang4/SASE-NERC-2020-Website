cd4f78064e6486d727ab0fd50a10181f1b14e4af
Jonathan Dang <jdang@wpi.edu>
2019-11-01 13:40:32 -0400

    got rid of the active highlighting

--- public/scripts.js
+++ public/scripts.js
@@ -3,10 +3,12 @@ $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
 
+/*
 $( '.navbar-nav a' ).on( 'click', function () {
 	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 	$( this ).parent( 'li' ).addClass( 'active' );
 });
+*/
 
 $(document).ready(function(){
     $("SplashLogo").fadeIn(5000);
