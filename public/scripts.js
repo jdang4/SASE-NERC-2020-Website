c84117567b6b3c6dd0fcc941eadbd2e34fcc7fde
Jonathan Dang <jdang@wpi.edu>
2019-11-02 22:13:00 -0400

    made the edits assigned on 11/2

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,5 @@
 
-$('.navbar-collapse a').click(function(){
+$('.navbar-collapse a').click(function () {
     $(".navbar-collapse").collapse('hide');
 });
 
@@ -10,22 +10,23 @@ $( '.navbar-nav a' ).on( 'click', function () {
 });
 */
 
-$(document).ready(function(){
+$(document).ready(function () {
     $("SplashLogo").fadeIn(5000);
 });
 
-$(document).ready(function(){
-  $('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-  });
+$(document).ready(function () {
+    var offset = -50;
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top + offset
+            }, 1000);
+        }
+    });
 });
- 
+
 
 $("a").removeClass("waves-effect waves-light");
 
